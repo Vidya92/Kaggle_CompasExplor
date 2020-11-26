fd74c346f1f767fb0cee5bea27b1ed335dd8c632
Vidya92 <50328387+Vidya92@users.noreply.github.com>
2020-11-25 22:09:49 -0500

    Update RacialBias.py

--- RacialBias.py
+++ RacialBias.py
@@ -26,39 +26,23 @@ import random as r
 import seaborn as sb
 import datetime as dt 
 
-Compass= pd.read_csv("C:/Users/17324/OneDrive/Documents/PythonScripts/RacialBias/CompasData.csv",encoding= 'unicode_escape')
-
+Compas Data 
 #let's make everything uppercase 
 Compass['FirstName'] = Compass['FirstName'].str.upper()
 Compass['LastName'] = Compass['LastName'].str.upper()
-Compass['Birthday'] = pd.to_datetime(Compass['Birthday'],infer_datetime_format=True,errors='coerce')
+Compass['Birthday'] = pd.to_datetime(Compass['Birthday'],infer_datetime_format=True,errors='coerce') #make sure everything has the same data format 
 
-SubsetViol= pd.read_csv("C:/Users/17324/OneDrive/Documents/PythonScripts/RacialBias/Violent.csv",encoding= 'unicode_escape')
-
+More info Risk of Violence Data 
 #let's make everything uppercase 
 SubsetViol['FirstName'] = SubsetViol['FirstName'].str.upper()
 SubsetViol['LastName'] = SubsetViol['LastName'].str.upper()
 
 SubsetViol['Birthday'] = pd.to_datetime(SubsetViol['Birthday'],infer_datetime_format=True,errors='coerce')
-#let's make everything uppercase 
 
+#Merge the data by 
 Data = pd.merge(Compass, SubsetViol, how='left', on=['FirstName', 'LastName','Birthday'])
 Data= pd.read_csv("C:/Users/17324/OneDrive/Documents/PythonScripts/RacialBias/CombinedData.csv",encoding= 'unicode_escape')
 
 #export_csv = Data.to_csv (r'C:/Users/17324/OneDrive/Documents/PythonScripts/RacialBias/CombinedData.csv', index = None, header=True)
 
 
-Data.dtypes
-
-Data.info()
-#Predict liklihood of taking a personal loan! 
-#Cleaning 
-
-Data['Ethnic_Code_Text'] = Data['Ethnic_Code_Text'].replace('African-Am','African-American')
-Data=Data[(Data['Ethnic_Code_Text'] != 'Other') & (Data['Ethnic_Code_Text'] != "Oriental")]
-
-RiskV= Data[(Data["DisplayText"] =='Risk of Violence')]
-
-sb.catplot(x="RecSupervisionLevelText", kind="count",hue='Ethnic_Code_Text', data=RiskV)
-
-sb.catplot(x="Ethnic_Code_Text" , y="DecileScore" ,kind="bar", data=Data)
